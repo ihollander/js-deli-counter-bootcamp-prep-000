24796f254ef7f46427052dfadc628de221fa4df3
Ian Hollander <ianholla@gmail.com>
2018-08-03 21:47:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = []
 
 function takeANumber(line, newPerson) {
-  var position = line.length
+  var position = line.length + 1
   return `Welcome, ${newPerson}. You are number ${position} in line.`
 }
 

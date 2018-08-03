55b59cc5c4e4d1ae0acaac99fabb6e3a33dcac6a
Ian Hollander <ianholla@gmail.com>
2018-08-03 21:52:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-function takeANumber(katzDeliLine, newPerson) {
-  katzDeliLine.push(newPerson)
-  var position = katzDeliLine.length
+function takeANumber(line, newPerson) {
+  line.push(newPerson)
+  var position = line.length
   return `Welcome, ${newPerson}. You are number ${position} in line.`
 }
 
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
+function nowServing(line) {
+  if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    var currentCustomer = katzDeliLine[0]
-    katzDeliLine.shift()
+    var currentCustomer = line[0]
+    line.shift()
     return currentCustomer
+  }
+}
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var lineText = "The line is currently: "
   }
 }
